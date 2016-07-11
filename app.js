42aa3174d91d8266f798edb82735e444b9fddcf9
Machiraju Venkata Naga Nithesh <Machiraju Venkata Naga Nithesh>
2016-07-11 12:45:17 +0530

    npm

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var departments = require('./routes/departments.js');
 var branches = require('./routes/branches.js');
+var doctorslist = require('./routes/doctorslist.js');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/departments', departments);
 app.use('/branches', branches);
+app.use('/doctorslist', doctorslist);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
