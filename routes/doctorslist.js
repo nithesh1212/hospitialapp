b3f3b3892dbfada13aeedf56c22e2069a59d10eb
Machiraju Venkata Naga Nithesh <Machiraju Venkata Naga Nithesh>
2016-07-11 13:13:21 +0530

    sc

--- routes/doctorslist.js
+++ routes/doctorslist.js
@@ -42,4 +42,47 @@ router.get('/', function(req, res, next) {
   res.send(obj);
 });
 
+router.get('/:name/branches', function(req, res, next){
+var obj = {
+    "Doctors":  [
+            {
+                "doctorname": "S.Sandeep",
+                "departments": "Cardiology",
+                "branch":["Kondapur","Gachibowli","Somajiguda"],
+                "id": "card001",
+            },
+             {
+                "doctorname": "M.Jagan",
+                "departments": "Neurology",
+                "branch":["Kondapur","Gachibowli","Filmnagar"],
+                "id": "card001",
+            },
+           {
+                "doctorname": "M.Ramakrishna",
+                "departments": "Endrocrinology",
+                "branch":["Kondapur","Secundrabad","Somajiguda"],
+                "id": "card001",
+            },
+             {
+                "doctorname": "G.Anil",
+                "departments": "Nephrology",
+                "branch":["Filmnagar","Secundrabad","Somajiguda"],
+                "id": "card001",
+            },
+             {
+                "doctorname": "M.Nithesh",
+                "departments": "E.N.T",
+                "branch":["Kondapur","Gachibowli","Somajiguda"],
+                "id": "card001",
+            },
+        ]
+}
+var doname =req.params.name;
+obj.Doctors.map(function(doctor){
+    if(doctor.doctorname === doname){
+        res.send(doctor);
+    }
+})
+});
+
 module.exports = router;
