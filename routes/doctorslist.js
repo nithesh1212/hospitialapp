12fe25aac735cd00f439705b854211ea993a7f41
Machiraju Venkata Naga Nithesh <Machiraju Venkata Naga Nithesh>
2016-07-11 12:57:44 +0530

    doctors.js

--- routes/doctorslist.js
+++ routes/doctorslist.js
@@ -9,31 +9,31 @@ router.get('/', function(req, res, next) {
             {
                 "doctorname": "S.Sandeep",
                 "departments": "Cardiology",
-                "branch":"{Kondapur,Gachibowli,Somajiguda}",
+                "branch":["Kondapur","Gachibowli","Somajiguda"],
                 "id": "card001",
             },
              {
                 "doctorname": "M.Jagan",
                 "departments": "Neurology",
-                "branch":"{Kondapur,Gachibowli,Filmnagar}",
+                "branch":["Kondapur","Gachibowli","Filmnagar"],
                 "id": "card001",
             },
            {
                 "doctorname": "M.Ramakrishna",
                 "departments": "Endrocrinology",
-                "branch":"{Kondapur,Secundrabad,Somajiguda}",
+                "branch":["Kondapur","Secundrabad","Somajiguda"],
                 "id": "card001",
             },
              {
                 "doctorname": "G.Anil",
                 "departments": "Nephrology",
-                "branch":"{Filmnagar,Secundrabad,Somajiguda}",
+                "branch":["Filmnagar","Secundrabad","Somajiguda"],
                 "id": "card001",
             },
              {
                 "doctorname": "M.Nithesh",
                 "departments": "E.N.T",
-                "branch":"{Kondapur,Gachibowli,Somajiguda}",
+                "branch":["Kondapur","Gachibowli","Somajiguda"],
                 "id": "card001",
             },
         ]
