7cddac6e245251432b716dd5b794e7d749de051c
Machiraju Venkata Naga Nithesh <Machiraju Venkata Naga Nithesh>
2016-07-12 16:37:01 +0530

    FC

--- routes/branches.js
+++ routes/branches.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Promise = require('bluebird');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,30 +10,35 @@ router.get('/', function(req, res, next) {
             {
                 "branchname": "Kondapur",
                 "numberofdepartments": "12",
-                "id": "kims001",
+                "lat":"17.46221",
+                "long":"78.35685"
             },
              {
                 "branchname": "Gachibowli",
                 "numberofdepartments": "15",
-                "id": "kims002",
+                "lat":"17.44008",
+                "long":"78.34892",
                 
             },
              {
                 "branchname": "Somajiguda",
                 "numberofdepartments": "18",
-                "id": "kims003",
+                "lat":"17.41418",
+                "long":"78.41169"
                 
             },
              {
                 "branchname": "Film Nagar",
                 "numberofdepartments": "16",
-                "id": "kiimms004",
+                "lat":"17.46221",
+                "long":"78.35685"
                
             },
              {
                 "branchname": "Secundrabad",
                "numberofdepartments": "12",
-                "id": "kims005",
+                "lat":"17.43993",
+                "long":"78.49827"
                 
             },
         ]
@@ -42,3 +48,94 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+
+router.get('/:location', function(req, res, next)
+ {
+    var obj =
+     {
+    "Branches": 
+         [
+            {
+                "branchname": "Kondapur",
+                "numberofdepartments": "12",
+                "lat":"17.46221",
+                "long":"78.35685"
+            },
+
+             {
+                "branchname": "Gachibowli",
+                "numberofdepartments": "15",
+                "lat":"17.44008",
+                "long":"78.34892"
+             },
+
+             {
+                "branchname": "Somajiguda",
+                "numberofdepartments": "18",
+                "lat":"17.41418",
+                "long":"78.41169"
+                
+            },
+             {
+                "branchname": "Film Nagar",
+                "numberofdepartments": "16",
+                "lat":"17.46221",
+                "long":"78.35685"
+               
+            },
+             {
+                "branchname": "Secundrabad",
+               "numberofdepartments": "12",
+                "lat":"17.43993",
+                "long":"78.49827"
+                
+            },
+        ]
+    }
+    
+
+  
+  return new Promise(function (resolve, reject) 
+  {
+        function distance(lat1, lon1, lat2, lon2, unit) 
+        {
+            
+            var radlat1 = Math.PI * lat1/180
+            var radlat2 = Math.PI * lat2/180
+            var theta = lon1-lon2
+            var radtheta = Math.PI * theta/180
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            dist = Math.acos(dist)
+            dist = dist * 180/Math.PI
+            dist = dist * 60 * 1.1515
+            if (unit=="K") { dist = dist * 1.609344 }
+            if (unit=="N") { dist = dist * 0.8684 }
+            return dist
+        }
+
+        var location =req.params.location;
+        var commasep = location.split(',');
+        var temp = {"Branches":[] }
+
+        obj.Branches.map(function(branch){
+            
+        var dist = distance(commasep[0],commasep[1],branch.lat,branch.long,'K');
+        
+        if(dist<2)
+            {
+            temp.Branches.push(branch);
+            
+            }
+        });
+     
+     res.send(temp);
+});
+    
+});
+
+
+module.exports = router;
+
